24529cfa2a65af73caca088b92ed7e1545e44fe4
karthik_3030@yahoo.com <karthik_3030@yahoo.com>
2018-10-22 20:13:29 +0530

    renamed variables

--- src/app/explore-search/explore-search.component.ts
+++ src/app/explore-search/explore-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ChangeDetectorRef  } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { observable } from 'rxjs'
 import { FormControl } from '@angular/forms';
 import { AzsearchService } from './azsearch.service';
@@ -6,7 +6,7 @@ import { pluck, map } from 'rxjs/operators';
 import { from } from 'rxjs';
 import { MatTableDataSource } from '@angular/material/table';
 
-export interface PeriodicElement {
+export interface RealEstate {
   street: string;
   source: number;
   city: number;
@@ -14,18 +14,7 @@ export interface PeriodicElement {
   status: string
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { source: 1, street: 'Hydrogen', city: 1.0079, status: 'H', description: '' },
-  { source: 2, street: 'Helium', city: 4.0026, status: 'He', description: '' },
-  { source: 3, street: 'Lithium', city: 6.941, status: 'Li', description: '' },
-  { source: 4, street: 'Beryllium', city: 9.0122, status: 'Be', description: '' },
-  { source: 5, street: 'Boron', city: 10.811, status: 'B', description: 'sdfafda' },
-  { source: 6, street: 'Carbon', city: 12.0107, status: 'C', description: '' },
-  { source: 7, street: 'Nitrogen', city: 14.0067, status: 'N', description: '' },
-  { source: 8, street: 'Oxygen', city: 15.9994, status: 'O', description: '' },
-  { source: 9, street: 'Fluorine', city: 18.9984, status: 'F', description: '' },
-  { source: 10, street: 'Neon', city: 20.1797, status: 'Ne', description: '' },
-];
+
 
 @Component({
   selector: 'app-explore-search',
@@ -37,10 +26,10 @@ export class ExploreSearchComponent implements OnInit {
   myControl = new FormControl();
   options: string[] = [];
   displayedColumns: string[] = ['source', 'street', 'city', 'description', 'status'];
-  dataSource = new MatTableDataSource<PeriodicElement>();
-  changesrc:PeriodicElement[] = []
+  dataSource = new MatTableDataSource<RealEstate>();
+  changesrc: RealEstate[] = []
 
-  constructor(private azSearchService: AzsearchService,private changeDetectorRefs: ChangeDetectorRef) { }
+  constructor(private azSearchService: AzsearchService, private changeDetectorRefs: ChangeDetectorRef) { }
 
   ngOnInit() {
     console.log("Search works ?");
@@ -50,14 +39,14 @@ export class ExploreSearchComponent implements OnInit {
       console.log(data)
     });
   }
- 
+
   private extractDescription(data: any): any {
     return data.map(v => {
       this.options.push(v.description);
-      var data:PeriodicElement =  {
+      var data: RealEstate = {
         source: v.source, street: v.street, city: v.city, status: v.status, description: v.description
       };
-      
+
       this.changesrc.push(data);
       return v;
     });
